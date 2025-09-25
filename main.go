919630d202c146331c1a9573c803fd67791c8b9d
Yohanes <yohanesdahlan@gmail.com>
2025-09-25 13:44:05 +0700

    adding air, JSON Encoding

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/greet", greeting)
+type Customer struct {
+	Name string `json:"full_name"`
+	City string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
 
-	http.ListenAndServe(":8080", nil)
+func main() {
+	// define routes
+	http.HandleFunc("/greet", greeting)
+	http.HandleFunc("/customers", getAllCustomers)
+
+	//  starting server
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func greeting(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w,"Hello World!")
+	}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+		customers := []Customer {
+			{Name: "Yohanes", City: "Jakarta", Zipcode: "110075"},
+			{Name: "Huber", City: "Jakarta", Zipcode: "110075"},
+		}
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
 	}
