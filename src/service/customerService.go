473e04f98d898f5e99237e6c554dc5ed14e9dda7
Yohanes <yohanesdahlan@gmail.com>
2025-09-26 13:31:44 +0700

    dta transfer object from status 10 to text

--- src/service/customerService.go
+++ src/service/customerService.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"go.mod/src/domain"
+	"go.mod/src/dto"
 	"go.mod/src/errs"
 )
 
 type CustomerServices interface {
 	GetAllCustomer(string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
@@ -25,8 +26,14 @@ func (s DefaultCustomerService ) GetAllCustomer(status string) ([]domain.Custome
 	return s.repo.FindAll(status)
 }
 
-func (s DefaultCustomerService ) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService ) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err :=  s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	} 
+
+	response := c.ToDto()
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
